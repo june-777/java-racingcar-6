a95eca81a2fcc722d8339f1bee3ae00f8d41abd3
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 15:07:28 +0900

    feat(RacingCars): 다른 자동차 이름과 중복인지 검증하는 기능

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,11 +1,30 @@
 package racingcar.domain;
 
+import static racingcar.domain.exception.CarExceptionMessage.DUPLICATE_EXISTS;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RacingCars {
     private final List<Car> cars;
 
     public RacingCars(List<Car> cars) {
+        validateDuplicate(cars);
         this.cars = cars;
+    }
+
+    private void validateDuplicate(List<Car> cars) {
+        Set<Car> uniqueCars = new HashSet<>(cars);
+
+        if (cars.size() != uniqueCars.size()) {
+            throw new IllegalArgumentException(DUPLICATE_EXISTS.getMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "RacingCars" +
+                cars;
     }
 }
