c9c0aa62624345e3c116b885f5089fafe19b0e4c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 14:43:47 +0900

    feat(CarName): 자동차 이름에 쉼표가 존재하는지 검증하는 기능

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,8 +1,10 @@
 package racingcar.domain;
 
 import static racingcar.domain.exception.CarNameExceptionMessage.BLANK;
+import static racingcar.domain.exception.CarNameExceptionMessage.CONTAIN_WRONG_ELEMENT;
 
 public class CarName {
+    private static final String WRONG_ELEMENT = ",";
     private final String name;
 
     public CarName(String name) {
@@ -12,11 +14,18 @@ public class CarName {
 
     private void validate(String name) {
         validateBlank(name);
+        validateWrongExists(name);
     }
 
     private void validateBlank(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(BLANK.getMessage());
+        }
+    }
+
+    private void validateWrongExists(String name) {
+        if (name.contains(WRONG_ELEMENT)) {
+            throw new IllegalArgumentException(CONTAIN_WRONG_ELEMENT.getMessage());
         }
     }
 }
