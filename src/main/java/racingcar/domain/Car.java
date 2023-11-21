691c5b770796f210efa3bf29483472bbd756e41c
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 15:03:54 +0900

    feat(Car,CarName): equals, hashCode, toString 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Car {
     private final CarName name;
     private final int position;
@@ -7,5 +9,27 @@ public class Car {
     public Car(CarName name, int position) {
         this.name = name;
         this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name + "," + position + ")";
     }
 }
--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,8 +1,10 @@
 package racingcar.domain;
 
-import static racingcar.domain.exception.CarNameExceptionMessage.BLANK;
-import static racingcar.domain.exception.CarNameExceptionMessage.CONTAIN_WRONG_ELEMENT;
-import static racingcar.domain.exception.CarNameExceptionMessage.OUT_OF_LENGTH;
+import static racingcar.domain.exception.CarExceptionMessage.BLANK;
+import static racingcar.domain.exception.CarExceptionMessage.CONTAIN_WRONG_ELEMENT;
+import static racingcar.domain.exception.CarExceptionMessage.OUT_OF_LENGTH;
+
+import java.util.Objects;
 
 public class CarName {
     private static final String WRONG_ELEMENT = ",";
@@ -36,5 +38,27 @@ public class CarName {
         if (name.isEmpty() || name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException(OUT_OF_LENGTH.getMessage());
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CarName carName = (CarName) o;
+        return Objects.equals(name, carName.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
