77106c18ec23b2534ac34d72145bc1f65d622be4
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 15:52:21 +0900

    feat(CarExceptionMessage): 자동차 관련 예외 메시지 추가

--- src/main/java/racingcar/domain/exception/CarExceptionMessage.java
+++ src/main/java/racingcar/domain/exception/CarExceptionMessage.java
@@ -5,7 +5,8 @@ public enum CarExceptionMessage {
     BLANK("[ERROR] 자동차 이름은 공백이여선 안됩니다."),
     CONTAIN_WRONG_ELEMENT("[ERROR] 자동차 이름에 쉼표가 포함되선 안됩니다."),
     OUT_OF_LENGTH("[ERROR] 자동차 이름은 1 ~ 5글자여야 합니다."),
-    DUPLICATE_EXISTS("[ERROR] 자동차 이름은 서로 중복일 수 없습니다.");
+    DUPLICATE_EXISTS("[ERROR] 자동차 이름은 서로 중복일 수 없습니다."),
+    OUT_OF_TOTAL_COUNT("[ERROR] 자동차의 개수가 정해진 범위를 벗어났습니다.");
 
     private final String message;
 
