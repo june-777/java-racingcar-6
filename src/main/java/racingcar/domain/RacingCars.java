47495b981f8fbac4927cdf2d031a91188ada7c88
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 15:51:38 +0900

    feat(RacingCars): 전체 자동차 개수가 정해진 범위를 벗어났는지 검증하는 기능

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -1,16 +1,20 @@
 package racingcar.domain;
 
 import static racingcar.domain.exception.CarExceptionMessage.DUPLICATE_EXISTS;
+import static racingcar.domain.exception.CarExceptionMessage.OUT_OF_TOTAL_COUNT;
 
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class RacingCars {
+    private static final int MIN_CAR_COUNT = 2;
+    private static final int MAX_CAR_COUNT = 500;
     private final List<Car> cars;
 
     public RacingCars(List<Car> cars) {
         validateDuplicate(cars);
+        validateTotalCount(cars);
         this.cars = cars;
     }
 
@@ -19,6 +23,12 @@ public class RacingCars {
 
         if (cars.size() != uniqueCars.size()) {
             throw new IllegalArgumentException(DUPLICATE_EXISTS.getMessage());
+        }
+    }
+
+    private void validateTotalCount(List<Car> cars) {
+        if (cars.size() < MIN_CAR_COUNT || cars.size() > MAX_CAR_COUNT) {
+            throw new IllegalArgumentException(OUT_OF_TOTAL_COUNT.getMessage());
         }
     }
 
