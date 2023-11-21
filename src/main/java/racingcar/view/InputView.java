bc9387f2281bee258ee250ee867ad9bd553fe3a4
june-777 <wlwhswnsrl96@gmail.com>
2023-11-21 17:47:10 +0900

    feat(InputView): 시도할 횟수를 입력받는 기능

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import racingcar.view.reader.Reader;
 import racingcar.view.validator.CarNameInputValidator;
+import racingcar.view.validator.TotalRoundInputValidator;
 
 public class InputView {
 
@@ -14,20 +15,27 @@ public class InputView {
     }
 
     public List<String> readCarNames() {
-        System.out.println(InputMessage.CAR_NAME.message);
+        System.out.println(Message.CAR_NAME.message);
         String carNames = reader.readLine();
         CarNameInputValidator.validate(carNames);
         return Arrays.asList(carNames.split(","));
     }
 
-    private enum InputMessage {
+    public int readTotalRound() {
+        System.out.println(Message.TOTAL_ROUND.message);
+        String totalRound = reader.readLine();
+        TotalRoundInputValidator.validate(totalRound);
+        return Integer.parseInt(totalRound);
+    }
+
+    private enum Message {
         CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
 
         TOTAL_ROUND("시도할 회수는 몇회인가요?");
 
         private final String message;
 
-        InputMessage(String message) {
+        Message(String message) {
             this.message = message;
         }
     }
