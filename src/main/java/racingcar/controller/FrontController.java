05df1d8e1a49a5ba24d4e8a2eb2a09dea393af67
june-777 <wlwhswnsrl96@gmail.com>
2023-12-01 21:36:59 +0900

    fix: 예외 출력문 제거

--- src/main/java/racingcar/controller/FrontController.java
+++ src/main/java/racingcar/controller/FrontController.java
@@ -12,7 +12,9 @@ public class FrontController {
         try {
             racingCarController.process();
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] " + e.getMessage());
+            // 예외 출력에 대한 명시는 없음
+            // 해당 미션은 예외에 대한 핸들링이 존재하지 않으므로, 사실 FrontController로 분리하지 않아도 됨
+//            System.out.println("[ERROR] " + e.getMessage());
         }
     }
 }
